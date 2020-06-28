c1c5b373000aa8f390c41302dce7ee306791e1a3
vanessuniq <vanessuniq@gmail.com>
2020-06-28 11:42:18 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients, reject_if: :ingredient_exist
+
+  def ingredient_exist(attributes)
+    ingredients.find_by(name: attributes[:name], quantity: attributes[:quantity])
+  end
 end
